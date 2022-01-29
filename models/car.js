345682848c76460524042269bf203ecfbd964693
xavigrausebastian <xavier.grau@kreatings.com>
2022-01-29 16:19:55 +0100

    ADD required parameter to each CarChema element

--- models/car.js
+++ models/car.js
@@ -7,11 +7,11 @@ const mongoose = require('mongoose'); // src: https://www.npmjs.com/package/mong
 const Schema = mongoose.Schema;
 
 const CarSchema = new Schema({
-    brand: String,
-    model: String,
-    color: String,
-    power: Number,
-    price: { type: Number, default: 0 }
+    brand: {type: String, require: true},
+    model: {type: String, require: true},
+    color: {type: String, require: true},
+    power: {type: Number, require: true},
+    price: {type: Number, require: true}
 });
 
 module.exports = mongoose.model('Car', CarSchema);
